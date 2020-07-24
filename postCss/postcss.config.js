70ce90aa1bf937924304f203c1be1859814bafa1
satoshi <angelsb94@gmail.com>
2020-07-24 02:34:58 -0500

    colores clase

---
+++ postCss/postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    //require('autoprefixer')({
+    //  grid: true
+    //}),
+    require('postcss-cssnext')({
+      features: {
+        autoprefixer: {
+          grid: true,
+          flexbox: false,
+        },
+        customProperties: false,
+        calc: false,
+      }
+    })
+  ]
+}
